5e6d332b7c373ac6b949c8254d4f0c05ac43d698
Philippe SICABAIG <30959026+philnsicab@users.noreply.github.com>
2017-08-19 14:43:08 +0200

    Delete sawtoothwave.h

--- Source/waves/sawtoothwave.h
+++
@@ -1,9 +0,0 @@
-#ifndef sawtoothwave_H_INCLUDED
-#define sawtoothwave_H_INCLUDED
-void sawtoothWave(int frequency,VstInt32 sampleFrames);
-    //frequency is the frequency of the saw wave in Hertz
-    //length=number of samples or sampleFrames. For a duration of 1s, length=44100
-    //y is a vector which will be played with wavplay(y,44100);
-    //retourne un tableau de longueur sampleFrames
-
-#endif // SAWTOOTHWAVE_H_INCLUDED
