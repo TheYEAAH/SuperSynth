79a380ed3637175684c9741e72662c68f0738738
philnsicab <philnsicab@hotmail.com>
2019-01-08 18:03:40 +0100

    Removed unused files

--- Source/SliderComponent.h
+++ Source/SliderComponent.h
@@ -7,12 +7,12 @@
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Created with Projucer version: 5.1.1
+  Created with Projucer version: 5.4.1
 
   ------------------------------------------------------------------------------
 
-  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright (c) 2015 - ROLI Ltd.
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
 
   ==============================================================================
 */
@@ -28,15 +28,15 @@
 //==============================================================================
 /**
                                                                     //[Comments]
-    An auto-generated component, created by the Introjucer.
+    An auto-generated component, created by the Projucer.
 
     Describe your class and how it works here!
                                                                     //[/Comments]
 */
 class SliderComponent  : public Component,
-                         public SliderListener,
-                         public ComboBoxListener,
-                         public ButtonListener
+                         public Slider::Listener,
+                         public ComboBox::Listener,
+                         public Button::Listener
 {
 public:
     //==============================================================================
@@ -60,74 +60,74 @@ private:
     //[/UserVariables]
 
     //==============================================================================
-    ScopedPointer<TabbedComponent> tabbedComponent;
-    ScopedPointer<HyperlinkButton> hyperlinkButton;
-    ScopedPointer<GroupComponent> groupComponent2;
-    ScopedPointer<Slider> slider21;
-    ScopedPointer<Slider> slider22;
-    ScopedPointer<Slider> slider23;
-    ScopedPointer<Slider> slider24;
-    ScopedPointer<Label> label25;
-    ScopedPointer<Label> label26;
-    ScopedPointer<Label> label27;
-    ScopedPointer<Label> label28;
-    ScopedPointer<Label> label29;
-    ScopedPointer<Label> label30;
-    ScopedPointer<Slider> slider28;
-    ScopedPointer<GroupComponent> groupComponent3;
-    ScopedPointer<ComboBox> comboBox;
-    ScopedPointer<Slider> slider25;
-    ScopedPointer<Slider> slider26;
-    ScopedPointer<Slider> slider27;
-    ScopedPointer<Slider> slider29;
-    ScopedPointer<Label> label31;
-    ScopedPointer<Label> label32;
-    ScopedPointer<Label> label33;
-    ScopedPointer<Label> label34;
-    ScopedPointer<Label> label35;
-    ScopedPointer<Label> label36;
-    ScopedPointer<Label> label37;
-    ScopedPointer<Label> label38;
-    ScopedPointer<Label> label39;
-    ScopedPointer<Label> label40;
-    ScopedPointer<Label> label41;
-    ScopedPointer<Label> label42;
-    ScopedPointer<ToggleButton> toggleButton5;
-    ScopedPointer<ToggleButton> toggleButton6;
-    ScopedPointer<Slider> slider30;
-    ScopedPointer<Slider> slider31;
-    ScopedPointer<Slider> slider32;
-    ScopedPointer<Slider> slider33;
-    ScopedPointer<Slider> slider34;
-    ScopedPointer<Slider> slider35;
-    ScopedPointer<GroupComponent> groupComponent4;
-    ScopedPointer<Slider> slider2;
-    ScopedPointer<Slider> slider3;
-    ScopedPointer<Slider> slider4;
-    ScopedPointer<Label> label2;
-    ScopedPointer<Label> label3;
-    ScopedPointer<Label> label5;
-    ScopedPointer<GroupComponent> groupComponent6;
-    ScopedPointer<Label> label8;
-    ScopedPointer<ToggleButton> toggleButton2;
-    ScopedPointer<GroupComponent> groupComponent5;
-    ScopedPointer<Viewport> viewport;
-    ScopedPointer<ComboBox> comboBox2;
-    ScopedPointer<ComboBox> comboBox3;
-    ScopedPointer<Label> label4;
-    ScopedPointer<Label> label6;
-    ScopedPointer<Slider> slider5;
-    ScopedPointer<Label> label7;
-    ScopedPointer<ToggleButton> toggleButton3;
-    ScopedPointer<GroupComponent> groupComponent7;
-    ScopedPointer<Slider> slider6;
-    ScopedPointer<Slider> slider7;
-    ScopedPointer<Label> label9;
-    ScopedPointer<Label> label12;
-    ScopedPointer<Label> label13;
-    ScopedPointer<Label> label16;
-    ScopedPointer<Slider> slider10;
-    ScopedPointer<Slider> slider11;
+    std::unique_ptr<TabbedComponent> tabbedComponent;
+    std::unique_ptr<HyperlinkButton> hyperlinkButton;
+    std::unique_ptr<GroupComponent> groupComponent2;
+    std::unique_ptr<Slider> slider21;
+    std::unique_ptr<Slider> slider22;
+    std::unique_ptr<Slider> slider23;
+    std::unique_ptr<Slider> slider24;
+    std::unique_ptr<Label> label25;
+    std::unique_ptr<Label> label26;
+    std::unique_ptr<Label> label27;
+    std::unique_ptr<Label> label28;
+    std::unique_ptr<Label> label29;
+    std::unique_ptr<Label> label30;
+    std::unique_ptr<Slider> slider28;
+    std::unique_ptr<GroupComponent> groupComponent3;
+    std::unique_ptr<ComboBox> comboBox;
+    std::unique_ptr<Slider> slider25;
+    std::unique_ptr<Slider> slider26;
+    std::unique_ptr<Slider> slider27;
+    std::unique_ptr<Slider> slider29;
+    std::unique_ptr<Label> label31;
+    std::unique_ptr<Label> label32;
+    std::unique_ptr<Label> label33;
+    std::unique_ptr<Label> label34;
+    std::unique_ptr<Label> label35;
+    std::unique_ptr<Label> label36;
+    std::unique_ptr<Label> label37;
+    std::unique_ptr<Label> label38;
+    std::unique_ptr<Label> label39;
+    std::unique_ptr<Label> label40;
+    std::unique_ptr<Label> label41;
+    std::unique_ptr<Label> label42;
+    std::unique_ptr<ToggleButton> toggleButton5;
+    std::unique_ptr<ToggleButton> toggleButton6;
+    std::unique_ptr<Slider> slider30;
+    std::unique_ptr<Slider> slider31;
+    std::unique_ptr<Slider> slider32;
+    std::unique_ptr<Slider> slider33;
+    std::unique_ptr<Slider> slider34;
+    std::unique_ptr<Slider> slider35;
+    std::unique_ptr<GroupComponent> groupComponent4;
+    std::unique_ptr<Slider> slider2;
+    std::unique_ptr<Slider> slider3;
+    std::unique_ptr<Slider> slider4;
+    std::unique_ptr<Label> label2;
+    std::unique_ptr<Label> label3;
+    std::unique_ptr<Label> label5;
+    std::unique_ptr<GroupComponent> groupComponent6;
+    std::unique_ptr<Label> label8;
+    std::unique_ptr<ToggleButton> toggleButton2;
+    std::unique_ptr<GroupComponent> groupComponent5;
+    std::unique_ptr<Viewport> viewport;
+    std::unique_ptr<ComboBox> comboBox2;
+    std::unique_ptr<ComboBox> comboBox3;
+    std::unique_ptr<Label> label4;
+    std::unique_ptr<Label> label6;
+    std::unique_ptr<Slider> slider5;
+    std::unique_ptr<Label> label7;
+    std::unique_ptr<ToggleButton> toggleButton3;
+    std::unique_ptr<GroupComponent> groupComponent7;
+    std::unique_ptr<Slider> slider6;
+    std::unique_ptr<Slider> slider7;
+    std::unique_ptr<Label> label9;
+    std::unique_ptr<Label> label12;
+    std::unique_ptr<Label> label13;
+    std::unique_ptr<Label> label16;
+    std::unique_ptr<Slider> slider10;
+    std::unique_ptr<Slider> slider11;
     Path internalPath1;
 
 
