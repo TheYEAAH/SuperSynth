473cd59c4d09c43d5500785a2ea3fb9f4fdf46d5
philnsicab <philnsicab@hotmail.com>
2019-06-10 19:35:22 +0200

    Changed project name

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -42,7 +42,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "SimpleSuperSynth";
+    const char* const  projectName    = "SuperSynth";
     const char* const  companyName    = "YEAAH";
     const char* const  versionString  = "0.1.0";
     const int          versionNumber  = 0x100;
